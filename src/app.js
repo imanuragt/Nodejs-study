d39616074a23edcb77ea0af3c0690d81f09e5c90
imanuragt <63799723+imanuragt@users.noreply.github.com>
2021-08-29 14:04:42 +0530

    Initial commitss

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const pathLoacation = path.join(__dirname, '../views');
+app.use(express.static(pathLoacation));
+app.set('view engine', 'hbs');
+app.get('', (req, res) => {
+    res.render('index');
+});
+app.get('/about', (rea, res) => {
+    res.send('<h1>About Page <h1>')
+})
+
+app.listen(3000, () => {
+    console.log('Runing on port 3000');
+})
