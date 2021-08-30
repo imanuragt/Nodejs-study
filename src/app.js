a9d0062e385ed6f773b4fb9c733f0ff79884847b
imanuragt <63799723+imanuragt@users.noreply.github.com>
2021-08-30 22:12:54 +0530

    use index hbs and bind value in it

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ const pathLoacation = path.join(__dirname, '../views');
 app.use(express.static(pathLoacation));
 app.set('view engine', 'hbs');
 app.get('', (req, res) => {
-    res.render('index'); 
+    res.render('index', {
+        title:'Weather App',
+        page:'Home page'
+    }); 
 }); 
 app.get('/about', (rea, res) => {
     res.send('<h1>About Page <h1>')
-})
-
+});
 app.listen(3000, () => {
     console.log('Runing on port 3000');
 })
