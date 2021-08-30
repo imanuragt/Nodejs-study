c2cad4e50090a4ce6be39251317f65a7a201ee03
imanuragt <63799723+imanuragt@users.noreply.github.com>
2021-08-30 23:01:16 +0530

    added path

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const pathLoacation = path.join(__dirname, '../views');
-app.use(express.static(pathLoacation));
+const viewPath = path.join(__dirname, './templates');
+// const pathLoacation = path.join(__dirname, '../views');
+
+app.use(express.static(viewPath));
 app.set('view engine', 'hbs');
+app.set('views', viewPath);
 app.get('', (req, res) => {
     res.render('index', {
-        title:'Weather App',
-        page:'Home page'
-    }); 
-}); 
+        title: 'Weather App',
+        page: 'Home page'
+    });
+});
 app.get('/about', (rea, res) => {
-    res.send('<h1>About Page <h1>')
+    res.render('about', {
+        title: 'About',
+        About: 'About'
+    });
 });
 app.listen(3000, () => {
     console.log('Runing on port 3000');
